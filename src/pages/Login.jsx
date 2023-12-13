f8a4e0073e5458a2d44dcaf7f8f8ba57d27696be
putragabrielll <putragmahk@gmail.com>
2023-12-13 17:34:22 +0700

    update: Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom"
 // import LoginLogo from '../assets/image/login.png'
 import LogoCoffee from '../assets/image/logo-coffee.png'
+import { FaGoogle, FaFacebookF } from "react-icons/fa";
+
 
 const Login = () => {
     return (
@@ -26,11 +29,11 @@ const Login = () => {
                         <form className="flex flex-col w-3/4 gap-4" action="">
                             <div className="flex flex-col">
                                 <label className="font-semibold my-2" htmlFor="email">Email</label>
-                                <input className="h-12 border pl-5 rounded-md" type="email" name="email" placeholder="Enter Your Email" id="email"></input>
+                                <input className="h-12 border pl-5 rounded-md" type="email" name="email" placeholder="Enter Your Email" id="email" />
                             </div>
                             <div className="flex flex-col">
                                 <label className="font-semibold my-2" htmlFor="password">Password</label>
-                                <input className="h-12 border pl-5 rounded-md" type="password" name="password" placeholder="Enter Your Password" id="password"></input>
+                                <input className="h-12 border pl-5 rounded-md" type="password" name="password" placeholder="Enter Your Password" id="password" />
                             </div>
                             <div className="flex justify-end">
                                 <a href="/forgot-password.html" className="text-orange-500">Lupa Password?</a>
@@ -38,8 +41,23 @@ const Login = () => {
                             <button className="px-5 py-2 bg-orange-500 border border-orange-500 rounded-md" type="submit"><a href="index.html">Login</a></button>
                         </form>
 
-                        <div></div>
-                        <div></div>
+                        <div className="my-6">
+                            <p className="text-gray-600">Not Have An Account? &nbsp;<Link className="text-orange-500" to={"/register"}>Register</Link></p>
+                        </div>
+
+                        <div className="flex w-3/4 justify-between gap-4">
+                            <button className="flex-1 flex items-center justify-center border-white py-4 rounded-xl shadow-2xl">
+                                {/* <img className="h-6 w-6" src="/assets/facebook.png" alt="" /> */}
+                                <FaFacebookF />
+                                <div className="ml-4">Facebook</div>
+                            </button>
+                            <button className="flex-1 flex items-center justify-center border-white py-4 rounded-xl shadow-2xl">
+                                {/* <img className="h-6 w-6" src="/assets/google.png" alt="" /> */}
+                                <FaGoogle />
+                                <div className="ml-4">Google</div>
+                            </button>
+                        </div>
+                        
                     </div>
                 </div>
             </section>
