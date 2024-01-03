37195710fbed7d0643b2b2d5929449792ecc1c16
putragabrielll <putragmahk@gmail.com>
2024-01-03 16:57:45 +0700

    update: private route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import { Navigate } from "react-router-dom"
 
 const PrivateRoute = ({children}) => {
-    const [token, setToken] = React.useState(window.localStorage.getItem("token")) // cara 1 untuk ambil token.
-    // const {token} = React.useState(window.localStorage.getItem("token")) // cara 2 untuk ambil token.
+    // const [token, setToken] = React.useState(window.localStorage.getItem("token")) // cara 1 untuk ambil token.
+    const [token] = React.useState(window.localStorage.getItem("token")) // cara 2 untuk ambil token.
 
     if(token){
         return (
